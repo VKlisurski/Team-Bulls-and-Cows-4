dbd25753943f56ac1af362c1fe30d1b92bb93a1d
Svetoslav Georgiev <vodkatinibg@yahoo.com>
2014-06-30 21:26:47 +0300

    Player class -> added encapsulation and validation to name and attempts fields.

--- Bulls-and-Cows-4/Player.cs
+++ Bulls-and-Cows-4/Player.cs
@@ -1,21 +1,52 @@
 ﻿namespace BullsAndCowsGame
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
 
     public class Player : IComparable<Player>
     {
-        public Player(string playerName, int attempts)
+        private string name;
+        private int attempts;
+
+        public Player(string name, int attempts)
         {
-            this.Name = playerName;
+            this.Name = name;
             this.Attempts = attempts;
         }
 
-        public string Name { get; set; }
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name should not be null, empty or whitespaces only.");
+                }
 
-        public int Attempts { get; set; }
+                this.name = value;
+            }
+        }
+
+        public int Attempts
+        {
+            get
+            {
+                return this.attempts;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Attempts should be a positive number.");
+                }
+
+                this.attempts = value;
+            }
+        }
 
         public int CompareTo(Player other)
         {
