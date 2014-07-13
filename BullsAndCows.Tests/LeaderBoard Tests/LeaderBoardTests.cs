348a679b42cdf53262a488080cc687963ab578f5
viktor90 <viktor_vlk@abv.bg>
2014-07-13 23:42:15 +0300

    LeaderBoard tests added

---
+++ BullsAndCows.Tests/LeaderBoard Tests/LeaderBoardTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCowsGame;
+
+namespace BullsAndCows.Tests.LeaderBoard_Tests
+{
+    [TestClass]
+    public class LeaderBoardTests
+    {
+        [TestMethod]
+        public void DefaultCount()
+        {
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>();
+
+            int resultCount = testLeaderBoard.Count;
+
+            Assert.AreEqual(0, resultCount, "The default leaderBoard count should be 0");
+        }
+
+        [TestMethod]
+        public void SetMaxNumberOfItems()
+        {
+            int expectedMaxNum = 5;
+
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>(expectedMaxNum);
+
+            int maxNumOfItemsResult = testLeaderBoard.MaxNumberOfItems;
+
+            Assert.AreEqual(expectedMaxNum, maxNumOfItemsResult, 
+                "The expected Maximum Number of Items is {0} and the result is {1}", expectedMaxNum, maxNumOfItemsResult);
+        }
+
+        [TestMethod]
+        public void TestAddOnePerson()
+        {
+            Player testPlayer = new Player("Viktor", 3);
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>();
+
+            testLeaderBoard.Add(testPlayer);
+            int output = testLeaderBoard.Count;
+
+            Assert.AreEqual(1, output, "The expected output should be 1, but it is {0}", output);
+        }
+
+        [TestMethod]
+        public void TestAddMoreThanMaximumPeople()
+        {
+            int maxItems = 3;
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>(maxItems);
+
+            int peopleAddedCount = maxItems + 1;
+            for (int i = 0; i < peopleAddedCount; i++)
+            {
+                string playerName = "Player" + i;
+
+                testLeaderBoard.Add(new Player(playerName, i));
+            }
+
+            int result = testLeaderBoard.Count;
+            Assert.AreNotEqual(peopleAddedCount, result,
+                "The people added should be {0} and the result is {1}", maxItems, result);
+        }
+
+        [TestMethod]
+        public void TestMoveNext()
+        {
+            Player viktor = new Player("Viktor", 3);
+            Player niki = new Player("Niki", 2);
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>();
+
+            testLeaderBoard.Add(viktor);
+            testLeaderBoard.Add(niki);
+
+            testLeaderBoard.MoveNext();
+            string resultPlayerName = testLeaderBoard.Current.Name;
+
+            Assert.AreSame("Niki", resultPlayerName,
+                "The expected result should be Niki, because he has less attempts, but the output is {0}",
+                resultPlayerName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestCurrent()
+        {
+            Player viktor = new Player("Viktor", 3);
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>();
+
+            testLeaderBoard.Add(viktor);
+
+            Player throwExceptionExpected = testLeaderBoard.Current;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestDispose()
+        {
+            Player viktor = new Player("Viktor", 3);
+            Player niki = new Player("Niki", 2);
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>();
+
+            testLeaderBoard.Add(viktor);
+            testLeaderBoard.Add(niki);
+
+            testLeaderBoard.MoveNext();
+            testLeaderBoard.Dispose();
+            Player throwExceptionExpected = testLeaderBoard.Current;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestReset()
+        {
+            Player viktor = new Player("Viktor", 3);
+            Player niki = new Player("Niki", 2);
+            LeaderBoard<Player> testLeaderBoard = new LeaderBoard<Player>();
+
+            testLeaderBoard.Add(viktor);
+            testLeaderBoard.Add(niki);
+
+            testLeaderBoard.MoveNext();
+            testLeaderBoard.Reset();
+            Player throwExceptionExpected = testLeaderBoard.Current;
+        }
+    }
+}
